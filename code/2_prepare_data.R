500debce3508e02c6d64080c6d9a46ab3c9edf00
Jeremy Eberle <jwe4ec@virginia.edu>
2022-01-27 20:13:06 -0500

    Validate training completion in task_log WIP

--- code/2_prepare_data.R
+++ code/2_prepare_data.R
@@ -65,7 +65,7 @@ coach_completion <- read.csv("./data/temp/coach_completion.csv")
 dir.create("./docs")
 
 sink(file = "./docs/data_filenames.txt")
-list.files("./data", recursive = TRUE, full.names = FALSE)
+print(list.files("./data", recursive = TRUE, full.names = FALSE), width = 80)
 sink()
 
 # ---------------------------------------------------------------------------- #
@@ -264,9 +264,223 @@ length(coach_not_engage_ids) == 146
 
 
 
+# Confirm that "task_log" entries are the best indicator of completing training
+# for CBM conditions
+
+cbm_ids <- 
+  unique(dat$angular_training[dat$angular_training$conditioning %in% cbm_conditions, 
+                              "participant_id"])
+train_session <- paste0(c("first", "second", "third", "fourth", "fifth"), "Session")
+
+output_cbm <- data.frame(participant_id = rep(cbm_ids, each = length(train_session)),
+                         session = rep(train_session, length(cbm_ids)),
+                         n_unq_step_index_for_scenarios = NA)
+
+for (i in 1:length(cbm_ids)) {
+  for (j in 1:length(train_session)) {
+    output_cbm[output_cbm$participant_id == cbm_ids[i] & output_cbm$session == train_session[j],
+               "n_unq_step_index_for_scenarios"] <- 
+      length(unique(dat$angular_training[dat$angular_training$step_title == "scenario" &
+                                           dat$angular_training$participant_id == cbm_ids[i] &
+                                           dat$angular_training$session_and_task_info == train_session[j],
+                                         "step_index"]))
+  }
+}
+
+comp_s1_train_ids <- dat$task_log$participant_id[dat$task_log$task_name == "1"]
+comp_s2_train_ids <- dat$task_log$participant_id[dat$task_log$task_name == "2"]
+comp_s3_train_ids <- dat$task_log$participant_id[dat$task_log$task_name == "3"]
+comp_s4_train_ids <- dat$task_log$participant_id[dat$task_log$task_name == "4"]
+comp_s5_train_ids <- dat$task_log$participant_id[dat$task_log$task_name == "5"]
+
+comp_train_ids <- list(comp_s1_train_ids = comp_s1_train_ids,
+                       comp_s2_train_ids = comp_s2_train_ids,
+                       comp_s3_train_ids = comp_s3_train_ids,
+                       comp_s4_train_ids = comp_s4_train_ids,
+                       comp_s5_train_ids = comp_s5_train_ids)
+
+comp_train_ids_cbm_diff <- data.frame(session = train_session,
+                                      comp_train_ids_in_tl_not_at = NA,
+                                      comp_train_ids_in_at_not_tl = NA)
+
+for (i in 1:length(train_session)) {
+  comp_train_ids_cbm_diff[comp_train_ids_cbm_diff$session == train_session[i],
+                          "comp_train_ids_in_tl_not_at"] <-
+    paste(setdiff(dat$study$participant_id[dat$study$participant_id %in% comp_train_ids[[i]] &
+                                             dat$study$conditioning %in% cbm_conditions],
+                  output_cbm$participant_id[output_cbm$session == train_session[i] &
+                                              output_cbm$n_unq_step_index_for_scenarios == 40]),
+          collapse = ", ")
+  comp_train_ids_cbm_diff[comp_train_ids_cbm_diff$session == train_session[i],
+                          "comp_train_ids_in_at_not_tl"] <-
+    paste(setdiff(output_cbm$participant_id[output_cbm$session == train_session[i] &
+                                              output_cbm$n_unq_step_index_for_scenarios == 40], 
+            dat$study$participant_id[dat$study$participant_id %in% comp_train_ids[[i]] &
+                                       dat$study$conditioning %in% cbm_conditions]),
+          collapse = ", ")
+}
+
+comp_train_ids_cbm_diff
+
+#   TODO: Investigate discrepancies between "task_log" and "angular_training"
+
+comp_train_ids_cbm_in_tl_not_at <- 
+  paste(comp_train_ids_cbm_diff$comp_train_ids_in_tl_not_at, collapse = ", ")
+comp_train_ids_cbm_in_tl_not_at <- 
+  sort(unique(as.integer(unlist(strsplit(comp_train_ids_cbm_in_tl_not_at, split = ", ")))))
+
+cbm_diff_report <- output_cbm[output_cbm$participant_id %in% comp_train_ids_cbm_in_tl_not_at, ]
+
+#     TODO: See if "comp_train_ids_in_at_not_tl" is an issue
+
+
+
+
+#     TODO: Why does 602 in "HR_NO_COACH" have no data in "angular_training" despite
+#     having "1" in "task_log" at "firstSession"?
+
+# View(dat$angular_training[dat$angular_training$participant_id == 602, ])
+# View(dat$task_log[dat$task_log$participant_id == 602, ])
+# dat$study$conditioning[dat$study$participant_id == 602]
+
+
+
+
+
+#     TODO: 
+
+# No data in "angular_training" = 602
+# Lacks data for "secondSession" in "task_log" = 832
+# 
+# firstSession
+# 0 = 406, 465
+# 10 = 639
+# 39 = 384, 1496
+# secondSession
+# 25 = 639
+# 30 = 779
+# 37 = 768
+# 39 = 429, 666, 916, 1756
+# thirdSession
+# 5 = 832
+# 10 = 779
+# 13 = 639
+# 16 = 1659
+# 39 = 429, 572
+# fourthSession
+# 36 = 714
+# fifthSession
+# 4 = 832
+
+
+
+
+
+#     TODO: How was 832 in "HR_COACHING" given "5" in "task_log" at "fifthSession" 
+#     when they didn't complete 40 scenarios (only completed a few)? Also seems that
+#     S2 and S3 lacked a full complement of scenarios. Asked Henry/Dan on 1/20/22.
+#     S2 also has no data in "task_log".
+
+# View(dat$angular_training[dat$angular_training$participant_id == 832 &
+#                             dat$angular_training$session_and_task_info == "fifthSession", ])
+# View(dat$angular_training[dat$angular_training$participant_id == 832, ])
+# View(dat$task_log[dat$task_log$participant_id == 832, ])
+# dat$study$conditioning[dat$study$participant_id == 832]
+
+
+
+
+
+# Confirm that "task_log" entries are the best indicator of completing training
+# for psychoeducation condition
+
+ctl_ids <-
+  unique(dat$angular_training[dat$angular_training$conditioning %in% "CONTROL", 
+                              "participant_id"])
+
+last_step_title <- list(firstSession = "When Is Anxiety Impairing?",
+                        secondSession = "Anxiety Disorders",
+                        thirdSession = c("Maintenance", "Maintenance of Anxiety"),
+                        fourthSession = "Other Psychological Problems",
+                        fifthSession = c("Open to Others", "Being Open to Others"))
+
+output_ctl <- data.frame(participant_id = rep(ctl_ids, each = length(train_session)),
+                         session = rep(train_session, length(ctl_ids)),
+                         last_step_title_present = NA)
+
+for (i in 1:length(ctl_ids)) {
+  for (j in 1:length(train_session)) {
+    output_ctl[output_ctl$participant_id == ctl_ids[i] & output_ctl$session == train_session[j],
+               "last_step_title_present"] <- 
+      any(dat$angular_training[dat$angular_training$participant_id == ctl_ids[i] &
+                                 dat$angular_training$session_and_task_info == train_session[j],
+                               "step_title"] %in% last_step_title[[j]])
+  }
+}
+
+comp_train_ids_ctl_diff <- data.frame(session = train_session,
+                                      comp_train_ids_in_tl_not_at = NA,
+                                      comp_train_ids_in_at_not_tl = NA)
+
+for (i in 1:length(train_session)) {
+  comp_train_ids_ctl_diff[comp_train_ids_ctl_diff$session == train_session[i],
+                          "comp_train_ids_in_tl_not_at"] <-
+    paste(setdiff(dat$study$participant_id[dat$study$participant_id %in% comp_train_ids[[i]] &
+                                             dat$study$conditioning == "CONTROL"],
+                  output_ctl$participant_id[output_ctl$session == train_session[i] &
+                                              output_ctl$last_step_title_present == TRUE]),
+          collapse = ", ")
+  comp_train_ids_ctl_diff[comp_train_ids_ctl_diff$session == train_session[i],
+                          "comp_train_ids_in_at_not_tl"] <-
+    paste(setdiff(output_ctl$participant_id[output_ctl$session == train_session[i] &
+                                              output_ctl$last_step_title_present == TRUE],
+                  dat$study$participant_id[dat$study$participant_id %in% comp_train_ids[[i]] &
+                                             dat$study$conditioning == "CONTROL"]),
+          collapse = ", ")
+}
+
+comp_train_ids_ctl_diff
+
+#   TODO: Investigate discrepancies between "task_log" and "angular_training"
+
+comp_train_ids_ctl_in_tl_not_at <- 
+  paste(comp_train_ids_ctl_diff$comp_train_ids_in_tl_not_at, collapse = ", ")
+comp_train_ids_ctl_in_tl_not_at <- 
+  sort(unique(as.integer(unlist(strsplit(comp_train_ids_ctl_in_tl_not_at, split = ", ")))))
+
+ctl_diff_report <- output_ctl[output_ctl$participant_id %in% comp_train_ids_ctl_in_tl_not_at, ]
+
+#     TODO: See if "comp_train_ids_in_at_not_tl" is an issue
+
+# View(dat$angular_training[dat$angular_training$session_and_task_info == "secondSession" &
+#                             dat$angular_training$participant_id == 857, ])
+# View(dat$angular_training[dat$angular_training$session_and_task_info == "secondSession" &
+#                             dat$angular_training$participant_id == 961, ])
+
+
+
+
+#     TODO: Others in "diff_report"
+
+
+
+
+#     TODO: How was 1280 in "CONTROL" given "5" in "task_log" at "fifthSession" 
+#     when they didn't complete "Open to Others" (only completed "Introduction")?
+#     Asked Henry/Dan on 1/20/22.
+
+# View(dat$angular_training[dat$angular_training$participant_id == 1280 &
+#                             dat$angular_training$session_and_task_info == "fifthSession", ])
+# View(dat$angular_training[dat$angular_training$participant_id == 1280, ])
+# View(dat$task_log[dat$task_log$participant_id == 1280, ])
+dat$study$conditioning[dat$study$participant_id == 1280]
+
+
+
+
+
 # TODO: Compute number who completed S5 training by condition
 
-comp_s5_train_ids <- dat$task_log$participant_id[dat$task_log$task_name == "5"]
 length(comp_s5_train_ids) == 559
 
 nrow(dat$study[dat$study$conditioning %in% c("LR_TRAINING") &
@@ -277,83 +491,6 @@ nrow(dat$study[dat$study$conditioning %in% c("HR_COACH") &
                  dat$study$participant_id %in% comp_s5_train_ids, ]) == 131
 nrow(dat$study[dat$study$conditioning %in% c("CONTROL") &
                  dat$study$participant_id %in% comp_s5_train_ids, ]) == 154
-
-# TODO: Confirm that a "5" in "task_log" is the best indicator of completing
-# training at "fifthSession" for CBM conditions
-
-cbm_ids <- 
-  unique(dat$angular_training[dat$angular_training$conditioning %in% cbm_conditions, 
-                              "participant_id"])
-
-output_cbm <- data.frame(participant_id = cbm_ids,
-                     n_unq_step_index_for_scenarios = rep(NA, length(cbm_ids)))
-
-for (i in 1:length(cbm_ids)) {
-  output_cbm$n_unq_step_index_for_scenarios[i] <- 
-    length(unique(dat$angular_training[dat$angular_training$step_title == "scenario" &
-                                         dat$angular_training$participant_id == cbm_ids[i] &
-                                         dat$angular_training$session_and_task_info == "fifthSession",
-                                       "step_index"]))
-}
-
-table(output_cbm$n_unq_step_index_for_scenarios)
-
-setdiff(dat$study$participant_id[dat$study$participant_id %in% comp_s5_train_ids &
-                                   dat$study$conditioning %in% cbm_conditions], 
-        output_cbm$participant_id[output_cbm$n_unq_step_index_for_scenarios == 40])
-setdiff(output_cbm$participant_id[output_cbm$n_unq_step_index_for_scenarios == 40], 
-        dat$study$participant_id[dat$study$participant_id %in% comp_s5_train_ids &
-                                   dat$study$conditioning %in% cbm_conditions])
-
-#   TODO: How was 832 in "HR_COACHING" given "5" in "task_log" at "fifthSession" 
-#   when they didn't complete 40 scenarios (only completed a few)? Also seems that
-#   S2 and S3 lacked a full complement of scenarios. Asked Henry/Dan on 1/20/22.
-
-# View(dat$angular_training[dat$angular_training$participant_id == 832 &
-#                             dat$angular_training$session_and_task_info == "fifthSession", ])
-# View(dat$angular_training[dat$angular_training$participant_id == 832, ])
-# View(dat$task_log[dat$task_log$participant_id == 832, ])
-dat$study$conditioning[dat$study$participant_id == 832]
-
-
-
-
-
-# TODO: Confirm that a "5" in "task_log" is the best indicator of completing
-# training at "fifthSession" for psychoeducation condition
-
-ctl_ids <-
-  unique(dat$angular_training[dat$angular_training$conditioning %in% "CONTROL", 
-                              "participant_id"])
-
-output_ctl <- data.frame(participant_id = ctl_ids,
-                         step_title_open_to_others = rep(NA, length(ctl_ids)))
-
-for (i in 1:length(ctl_ids)) {
-  output_ctl$step_title_open_to_others[i] <- 
-    any(dat$angular_training[dat$angular_training$participant_id == ctl_ids[i],
-                             "step_title"] %in% c("Open to Others", "Being Open to Others"))
-}
-
-table(output_ctl$step_title_open_to_others)
-
-setdiff(dat$study$participant_id[dat$study$participant_id %in% comp_s5_train_ids &
-                                   dat$study$conditioning %in% "CONTROL"], 
-        output_ctl$participant_id[output_ctl$step_title_open_to_others == TRUE])
-setdiff(output_ctl$participant_id[output_ctl$step_title_open_to_others == TRUE], 
-        dat$study$participant_id[dat$study$participant_id %in% comp_s5_train_ids &
-                                   dat$study$conditioning %in% "CONTROL"])
-
-#   TODO: How was 1280 in "CONTROL" given "5" in "task_log" at "fifthSession" 
-#   when they didn't complete "Open to Others" (only completed "Introduction")?
-#   Asked Henry/Dan on 1/20/22.
-
-# View(dat$angular_training[dat$angular_training$participant_id == 1280 &
-#                             dat$angular_training$session_and_task_info == "fifthSession", ])
-# View(dat$angular_training[dat$angular_training$participant_id == 1280, ])
-# View(dat$task_log[dat$task_log$participant_id == 1280, ])
-dat$study$conditioning[dat$study$participant_id == 1280]
-
 
 
 
@@ -484,7 +621,8 @@ dat$participant[dat$participant$participant_id %in% s5_train_comp_anal_ids,
 # at actual device usage on 1/25/22.
 
 # Tables for substantive analysis:
-#   - "rr" (pos and neg bias), "bbsiq" (pos and neg bias), "oa" (anxiety)
+#   - "rr" (pos and neg bias), "bbsiq" (pos and neg bias)
+#   - "oa" and "dass21_as" (anxiety)
 #   - "task_log" or "angular_training" (training session completion)
 #   - "demographics" ("income", age computed from "birth_year")
 # Tables for baseline characteristics:
