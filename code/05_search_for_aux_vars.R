0718308845ef1f9f353a07177841cbe3907a6d08
Jeremy Eberle <jwe4ec@virginia.edu>
2022-04-05 11:26:48 -0400

    Collapse levels and test differences

--- code/05_search_for_aux_vars.R
+++ code/05_search_for_aux_vars.R
@@ -98,6 +98,18 @@ tmp_dem <- dat2$demographics[, c("participant_id", "education", "employment_stat
                                  "ethnicity", "gender", "marital_stat", "race_col",
                                  "country", "country_col")]
 
+# After inspection of differences in mean proportion of missing assessments (see
+# below), it was decided to collapse "gender" levels further
+
+tmp_dem$gender_col <- as.character(tmp_dem$gender)
+
+tmp_dem$gender_col[tmp_dem$gender_col %in%
+                     c("Transgender", "Transgender Female", "Transgender Male",
+                       "Other", "Prefer not to answer")] <- "Transgender/Other/Prefer not to answer"
+
+tmp_dem$gender_col <- factor(tmp_dem$gender_col,
+                               levels = c("Female", "Male", "Transgender/Other/Prefer not to answer"))
+
 # Extract training confidence and change importance items
 
 tmp_cred <- dat2$credibility[, c("participant_id", "confident_online", "important")]
@@ -150,6 +162,17 @@ tmp_tl_unq$device_col <-
          levels = c("desktop", "tablet", "mobile", "multiple types"))
 
 tmp_tl_unq2 <- unique(tmp_tl_unq[, c("participant_id", "n_devices", "device_col")])
+
+# After inspection of differences in mean proportion of missing assessments (see
+# below), it was decided to collapse "device_col" further into a binary variable
+
+tmp_tl_unq2$device_col_bin <- as.character(tmp_tl_unq2$device_col)
+
+tmp_tl_unq2$device_col_bin[tmp_tl_unq2$device_col_bin %in%
+                             c("desktop", "tablet", "mobile")] <- "one type"
+
+tmp_tl_unq2$device_col_bin <- factor(tmp_tl_unq2$device_col_bin,
+                                     levels = c("one type", "multiple types"))
 
 # Add extracted variables
 
@@ -251,11 +274,13 @@ compl_class_meas_compl <- compl_itt_iv[compl_itt_iv$class_meas_compl_anlys == 1,
 compute_desc_by_level <- function(df) {
   # Compute count, mean, and standard deviation
   
-  vars <- c("gender", "race_col", "ethnicity", "country_col", "education",
-            "employment_stat", "marital_stat", "device_col")
+  vars <- c("gender", "gender_col", "race_col", "ethnicity", "country_col", 
+            "education", "employment_stat", "marital_stat", 
+            "device_col", "device_col_bin")
   
-  var_labels <- c("Gender", "Race", "Ethnicity", "Country", "Education",
-                  "Employment Status", "Marital Status", "Device")
+  var_labels <- c("Gender", "Gender (Collapsed)", "Race", "Ethnicity", "Country", 
+                  "Education", "Employment Status", "Marital Status", 
+                  "Device (Collapsed)", "Device (Binary)")
   
   res <- data.frame()
   
@@ -301,6 +326,40 @@ dir.create("./results/search_aux_vars")
 write.csv(fct_res,
           file = "./results/search_aux_vars/potential_cat_ord_aux_vars.csv",
           row.names = FALSE)
+
+# Inspect table using Cynthia Tong's guidance on 4/1/22: For very small differences 
+# in mean proportion of missing assessments between levels of a potential auxiliary 
+# variable, exclude the variable from missing data handling. For clear differences, 
+# include the variable. If unsure, test differences and include the variable if
+# the differences are significant; otherwise, exclude it.
+
+# On 4/1/22, Cynthia Tong, Katie Daniel, and Jeremy Eberle deemed variables except
+# gender and device to have very small differences. Device was deemed to have clear 
+# differences; however, Cynthia advised that its levels be collapsed into a binary 
+# variable ("device_col_bin" above) to promote model convergence. We were unsure re
+# gender and decided to collapse sparse levels (above) and test differences (below).
+
+# Test gender differences using one-way ANOVA given that sample size of each level
+# is sufficiently large (i.e., 2-9 groups with >= 15 participants per group) see 
+# https://support.minitab.com/en-us/minitab-express/1/help-and-how-to/modeling-statistics/anova/how-to/kruskal-wallis-test/before-you-start/data-considerations/).
+# Given significant differences, include "gender_col" in missing data handling.
+
+sink(file = "./results/search_aux_vars/gender_col_differences.txt")
+
+cat("ITT Sample (Unrestricted):", "\n", "\n")
+anova(lm(compl_itt_unrestricted$miss_session_assess_prop ~ compl_itt_unrestricted$gender_col))
+cat("\n")
+cat("--------------------", "\n", "\n")
+
+cat("ITT Sample (Restricted):", "\n", "\n")
+anova(lm(compl_itt_restricted$miss_session_assess_prop ~ compl_itt_restricted$gender_col))
+cat("\n")
+cat("--------------------", "\n", "\n")
+
+cat("Classif. Meas. Compl. Sample:", "\n", "\n")
+anova(lm(compl_class_meas_compl$miss_session_assess_prop ~ compl_class_meas_compl$gender_col))
+
+sink()
 
 # ---------------------------------------------------------------------------- #
 # Search for time-invariant continuous auxiliary variables ----
@@ -401,3 +460,6 @@ num_res <- rbind(c("ITT Sample (Unrestricted)", rep(NA, ncol - 1)),
 write.csv(num_res,
           file = "./results/search_aux_vars/potential_num_aux_vars.csv",
           row.names = FALSE)
+
+# Inspect results using Cynthia Tong's guidance on 4/1/22. Given that none of the
+# correlations are significant, exclude all variables from missing data handling.
