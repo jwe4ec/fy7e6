cdd3e286435765a1f32d5db5a2b69b0ad15bc16c
Jeremy Eberle <jwe4ec@virginia.edu>
2023-02-14 14:19:42 -0500

    Add files from Jackie

---
+++ code/15d_test_concatenate_results.R
@@ -0,0 +1,53 @@
+# ---------------------------------------------------------------------------- #
+# Concatenate Results for "a1" Contrast Models Run in Parallel on Parallel Partition
+# Authors: Jackie Huband and Jeremy W. Eberle
+# ---------------------------------------------------------------------------- #
+
+# ---------------------------------------------------------------------------- #
+# Notes ----
+# ---------------------------------------------------------------------------- #
+
+# This script concatenates "results_list" files for a given model. It is run on
+# the standard partition via a separate Slurm script.
+
+# Before running script, restart R and set working directory to "code" folder
+
+# ---------------------------------------------------------------------------- #
+# Check number of "results_list" files for a model
+# ---------------------------------------------------------------------------- #
+
+test_num <- as.integer(commandArgs(trailingOnly=TRUE))
+results_path <- paste0("../a1_outputs/test_results_", test_num)
+cat("\nProcessing files in ", results_path, "\n")
+if (file.exists(results_path)){
+
+   files <- list.files(results_path, pattern = ".RData", full.names = TRUE)
+
+
+   # ---------------------------------------------------------------------------- #
+   # Concatenate "results_list" files into one list for the model
+   # ---------------------------------------------------------------------------- #
+
+   combined_list <- NULL
+
+   for (myfile in files) {
+     load(myfile)
+     combined_list <- c(combined_list, results_list)
+     rm(results_list)
+   }
+
+   print(length(combined_list))
+ 
+   # ---------------------------------------------------------------------------- #
+   # Complete work that was in the original code
+   # ---------------------------------------------------------------------------- #
+   model_results_path_stem <- combined_list[[1]]$model_results_path_stem
+
+   # Create results object
+
+   results <- list(per_bs_smp = combined_list)
+  
+
+   outfile <- paste0("results_", test_num, ".RData")
+   save(results, file = outfile)
+}
