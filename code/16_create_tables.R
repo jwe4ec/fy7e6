ff742f5d289a86e3fb63666c14005e784d23cd24
jwe4ec <jwe4ec@virginia.edu>
2023-03-01 16:31:26 -0500

    Add parameter labels

--- code/16_create_tables.R
+++ code/16_create_tables.R
@@ -29,15 +29,19 @@ groundhog_day <- version_control()
 # No packages loaded
 
 # ---------------------------------------------------------------------------- #
-# Import results ----
+# Import results and parameter labels ----
 # ---------------------------------------------------------------------------- #
 
 pooled_path <- "./results/bayesian/pooled/"
+param_path  <- "./code/parameter_labels/"
 
 load(paste0(pooled_path, "res_drp_c1_2000bs.RData"))
 load(paste0(pooled_path, "res_eff_c1_2000bs.RData"))
 load(paste0(pooled_path, "res_drp_c2_4_1000000iter.RData"))
 load(paste0(pooled_path, "res_eff_c2_4_1000000iter.RData"))
+
+eff_param_labels <- read.csv(paste0(param_path, "eff_param_labels.csv"))
+drp_param_labels <- read.csv(paste0(param_path, "drp_param_labels.csv"))
 
 # ---------------------------------------------------------------------------- #
 # Create full tables for "a1" models ----
@@ -45,17 +49,18 @@ load(paste0(pooled_path, "res_eff_c2_4_1000000iter.RData"))
 
 # Define function to create tables with full results for "a1" models
 
-create_full_tbl_a1 <- function(results, out_path) {
+create_full_tbl_a1 <- function(results, param_labels, out_path) {
   # Extract model info and pooled stats
   
   model_info        <- results$model_info
   pooled_conv_stats <- results$pooled_conv_stats
   
-  # TODO: Add column describing parameters
+  # Add parameter labels
   
+  pooled_conv_stats <- cbind(param = rownames(pooled_conv_stats), pooled_conv_stats)
   
-  
-  
+  pooled_conv_stats <- merge(pooled_conv_stats, param_labels, "param", 
+                             all.x = TRUE, sort = FALSE)
   
   # Identify significant parameters
   
@@ -81,7 +86,8 @@ create_full_tbl_a1 <- function(results, out_path) {
   
   pooled_conv_stats <- pooled_conv_stats[, !(names(pooled_conv_stats) %in% rm_cols)]
   
-  pooled_conv_stats <- pooled_conv_stats[, c("num_converge_all", "mean", "emp_sd",
+  pooled_conv_stats <- pooled_conv_stats[, c("param", "model", "label",
+                                             "num_converge_all", "mean", "emp_sd",
                                              "avg_sd", "pctl_bs_ci", "sig")]
   
   # TODO: Write to one PDF with multiple pages (for now export to CSV)
@@ -91,7 +97,7 @@ create_full_tbl_a1 <- function(results, out_path) {
                        model_info$a_contrast, "_",
                        model_info$y_var)
   
-  write.csv(pooled_conv_stats, paste0(out_path, model_name, ".csv"))
+  write.csv(pooled_conv_stats, paste0(out_path, model_name, ".csv"), row.names = FALSE)
   
   
   
@@ -106,16 +112,16 @@ full_results_tbl_path <- "./results/bayesian/tables/full/"
 
 dir.create(full_results_tbl_path, recursive = TRUE)
 
-lapply(res_drp_c1_2000bs, create_full_tbl_a1, full_results_tbl_path)
-lapply(res_eff_c1_2000bs, create_full_tbl_a1, full_results_tbl_path)
+lapply(res_drp_c1_2000bs, create_full_tbl_a1, drp_param_labels, full_results_tbl_path)
+lapply(res_eff_c1_2000bs, create_full_tbl_a1, eff_param_labels, full_results_tbl_path)
 
 # ---------------------------------------------------------------------------- #
-# TODO: Create full tables for "a2" models ----
+# Create full tables for "a2" models ----
 # ---------------------------------------------------------------------------- #
 
 # Define function to create tables with full results for "a2" models
 
-create_full_tbl_a2 <- function(results, out_path) {
+create_full_tbl_a2 <- function(results, param_labels, out_path) {
   # Combine statistics in data frame
   
   stats <- data.frame(mean      = results$summary$statistics[, "Mean"],
@@ -124,11 +130,11 @@ create_full_tbl_a2 <- function(results, out_path) {
                       hpd_ci_ul = results$hpd_interval[, "upper"],
                       geweke_z  = results$geweke$z)
   
-  # TODO: Add column describing parameters
+  # Add parameter labels
   
+  stats <- cbind(param = rownames(stats), stats)
   
-  
-  
+  stats <- merge(stats, param_labels, "param", all.x = TRUE, sort = FALSE)
   
   # Identify significant parameters
   
@@ -152,7 +158,8 @@ create_full_tbl_a2 <- function(results, out_path) {
   
   stats <- stats[, !(names(stats) %in% rm_cols)]
   
-  stats <- stats[, c("mean", "emp_sd", "hpd_ci", "geweke_z", "sig")]
+  stats <- stats[, c("param", "model", "label", 
+                     "mean", "emp_sd", "hpd_ci", "geweke_z", "sig")]
   
   # TODO: Write to one PDF with multiple pages (for now export to CSV)
   
@@ -161,7 +168,7 @@ create_full_tbl_a2 <- function(results, out_path) {
                        results$model_info$a_contrast, "_",
                        results$model_info$y_var)
   
-  write.csv(stats, paste0(out_path, model_name, ".csv"))
+  write.csv(stats, paste0(out_path, model_name, ".csv"), row.names = FALSE)
   
   
   
@@ -172,8 +179,8 @@ create_full_tbl_a2 <- function(results, out_path) {
 
 # Run function (use same directory used for "a1" above)
 
-lapply(res_drp_c2_4_1000000iter, create_full_tbl_a2, full_results_tbl_path)
-lapply(res_eff_c2_4_1000000iter, create_full_tbl_a2, full_results_tbl_path)
+lapply(res_drp_c2_4_1000000iter, create_full_tbl_a2, drp_param_labels, full_results_tbl_path)
+lapply(res_eff_c2_4_1000000iter, create_full_tbl_a2, eff_param_labels, full_results_tbl_path)
 
 # ---------------------------------------------------------------------------- #
 # TODO: Create summary table for "a1" models ----
